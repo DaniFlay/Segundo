b80763255b9efcbc2e7d42a2bb6136b16dea879a
DaniFlay <64229951+DaniFlay@users.noreply.github.com>
2023-10-03 14:05:51 +0200

    Add files via upload

---
+++ ConversorMonedas/src/main/java/com/mycompany/conversormonedas/ConversorMonedas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversormonedas;
+
+/**
+ *
+ * @author EstudianteDAM204
+ */
+public class ConversorMonedas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
