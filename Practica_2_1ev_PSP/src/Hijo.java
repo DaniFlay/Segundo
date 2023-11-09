ce3527e5d196ad40ab83c9a68215e5e87f1c504f
Daniel <superdemix97@gmail.com>
2023-11-09 14:04:30 +0100

    psp

--- Practica_2_1ev_PSP/src/Hijo.java
+++ Practica_2_1ev_PSP/src/Hijo.java
@@ -5,23 +5,24 @@ import java.io.InputStreamReader;
 public class Hijo {
 
 	public static void main(String[] args) {
-		String comandoDir= "dir";
-		String comandoFind="find "+args[0]+" *";
+		String comando; 
+		if(args.length==0){
+			comando= "dir";
+		}
+		else{
+			comando="find \""+args[0]+"\" *";
+		}
+		
 		
 		try {
-			Process p= new ProcessBuilder("cmd","/c",comandoDir).start();
-			Process p2= new ProcessBuilder("cmd","/c",comandoFind).start();
+			Process p= new ProcessBuilder("cmd","/c",comando).start();
+
 			BufferedReader bf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader bf2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));
 			String linea="";
-			System.out.println("Proceso comando DIR:");
 			while((linea=bf.readLine())!=null) {
 				System.out.println(linea);
 			}
-			System.out.println("Proceso comando find: ");
-			while((linea= bf2.readLine())!=null){
-				System.out.println(linea);
-			}
+	
 		} catch (IOException e) {
 			
 			e.printStackTrace();
--- Practica_2_1ev_PSP/src/Padre.java
+++ Practica_2_1ev_PSP/src/Padre.java
@@ -6,17 +6,15 @@ public class Padre {
 
 	public static void main(String[] args) {
 		String dir= "C:\\Users\\EstudianteDAM204\\Desktop\\gitSegundo\\Practica_2_1ev_PSP\\src\\Hijo.java";
-		String find= "C:\\Users\\EstudianteDAM204\\Desktop\\gitSegundo\\Practica_2_1ev_PSP\\src\\Hijo.java find "+args[0]+ " *";
+		String comando="";
+		if(args.length>0){
+			comando+=args[0];
+		}
 		try {
-			Process p1= new ProcessBuilder("java",dir,"dir").start();
-			Process p2= new ProcessBuilder("java",dir, find).start();
+			Process p1= new ProcessBuilder("java",dir,comando).start();
 			BufferedReader bf= new BufferedReader(new InputStreamReader(p1.getInputStream()));
-			BufferedReader bf2= new BufferedReader(new InputStreamReader(p2.getInputStream()));
 			String linea="";
 			while((linea=bf.readLine())!=null){
-				System.out.println(linea);
-			}
-			while ((linea=bf2.readLine())!=null) {
 				System.out.println(linea);
 			}
 		} catch (IOException e) {
